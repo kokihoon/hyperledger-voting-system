e50ceb874003910ce6167512b143d1fc67c3087c
5pecia1 <5o1.3an9@gmail.com>
2017-11-27 17:36:21 +0900

    make login

---
+++ main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import login, authenticate
+
+def index(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return HttpResponse('로그인 실패. 다시 시도 해보세요.')
+    else:
+        return render(request, 'main/login.html')
+
