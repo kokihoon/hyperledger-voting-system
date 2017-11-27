d7e4351c4001e1fb2c4e2e3470341a051c4c163e
5pecia1 <5o1.3an9@gmail.com>
2017-11-27 19:38:55 +0900

    update user check

--- main/views.py
+++ main/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth import login, authenticate
 
+from .models import UserCheck
+
 def index(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
+
         if user is not None:
             login(request, user)
-            return redirect('index')
+            checkedUser = UserCheck.objects.filter(user=user)
+
+            if checkedUser:
+                # TODO change about check
+                print('checked user')
+                return redirect('index')
+            else:
+                UserCheck.objects.create(user=user)
+                print('add user')
+                return redirect('index')
         else:
             return HttpResponse('로그인 실패. 다시 시도 해보세요.')
     else:
