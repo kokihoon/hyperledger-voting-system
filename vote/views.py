1f55b6df8fef833733a86891e64e97e228b0087b
5pecia1 <5o1.3an9@gmail.com>
2017-11-28 12:24:58 +0900

    update access denied

--- vote/views.py
+++ vote/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import logout, get_user
 
 def index(request):
-    return render(request, 'vote/vote.html')
+    # print(get_user(request))
+    rend = render(request, 'vote/vote.html', {logout: logout, request: request})
+    logout(request)
+    return rend
 
