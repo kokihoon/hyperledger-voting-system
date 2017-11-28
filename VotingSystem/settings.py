99d87ee8bd1ff22938230e236fffb2e4441b156c
5pecia1 <5o1.3an9@gmail.com>
2017-11-28 22:52:37 +0900

    add to make member

--- VotingSystem/settings.py
+++ VotingSystem/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+import secrect_setting
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -121,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DOMAIN_URL = secrect_setting.DOMAIN_URL
+DOMAIN_API_URL = DOMAIN_URL + "/api"
+MEMBER_URL = DOMAIN_API_URL + "/Member"
--- vote/views.py
+++ vote/views.py
@@ -1,9 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.contrib.auth import logout, get_user
 
-def index(request):
-    # print(get_user(request))
-    rend = render(request, 'vote/vote.html', {logout: logout, request: request})
-    logout(request)
-    return rend
+import hashlib
+from datetime import datetime
 
+from VotingSystem.settings import MEMBER_URL
+
+import requests
+
+
+def index(request):
+    user_id = get_id(get_user(request))
+    request_json = get_number_json(user_id)
+    r = requests.post(
+        MEMBER_URL,
+        json=request_json
+    )
+
+    if r.status_code == 200:
+        rend = render(request, 'vote/vote.html', {'user_id': user_id})
+        logout(request)
+        return rend
+    else:
+        return HttpResponse('로그인 실패. 다시 시도 해보세요.')
+
+
+def get_id(user) -> str:
+    seed_data = str(user) + str(datetime.now().time())
+    return hashlib.sha3_256(seed_data.encode()).hexdigest()
+
+
+def get_number_json(user_id):
+    return {
+        "$class": "org.acme.vehicle.auction.Member",
+        "balance": 1,
+        "email": user_id,
+        "firstName": user_id,
+        "lastName": user_id
+    }
