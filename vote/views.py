024df93344e2b2d0b78ccb3eb5d4c20800f6d62f
kokihoon <rhrlgns777@gmail.com>
2017-11-28 22:53:01 +0900

    dynamic table

--- vote/views.py
+++ vote/views.py
@@ -4,6 +4,6 @@ from django.contrib.auth import logout, get_user
 def index(request):
     # print(get_user(request))
     rend = render(request, 'vote/vote.html', {logout: logout, request: request})
-    logout(request)
+    # logout(request)
     return rend
 
